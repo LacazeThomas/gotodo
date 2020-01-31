176141b025ea33623771c0522578de408f61741f
Thomas LACAZE <contact@thomaslacaze.fr>
2020-01-31 15:50:41 +0100

    fixe token

--- app/handler/login.go
+++ app/handler/login.go
@@ -42,7 +42,7 @@ func Authenticate(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
 	}
 	
 	type login struct {
-		Token string
+		Token string `json:"token"`
 	}
 
 	respondJSON(w, http.StatusOK, login{resp.Token})
